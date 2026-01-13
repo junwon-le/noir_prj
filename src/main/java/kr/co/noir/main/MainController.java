66047d8259ef2e5cdd87dd167a7bcc127720946e
jy9s <jyeppp@gmail.com>
2026-01-13 16:53:02 +0900

    html로 변경

--- src/main/java/kr/co/noir/main/MainController.java
+++ src/main/java/kr/co/noir/main/MainController.java
@@ -2,14 +2,30 @@ package kr.co.noir.main;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("main")
 @Controller
 public class MainController {
 	
-	@GetMapping("test")
+	@GetMapping("/main")
 	public String Main() {
-		return "test";
+		return "/main";
+	}
+	
+	@GetMapping("/room")
+	public String Room() {
+		return "/room/room";
+	}
+	@GetMapping("/roomDetail")
+	public String RoomDetail() {
+		return "/room/roomDetail";
+	}
+	
+	@GetMapping("/dinning")
+	public String Dinning() {
+		return "/dinning/dinning";
+	}
+	@GetMapping("/dinningDetail")
+	public String DinningDetail() {
+		return "/dinning/dinningDetail";
 	}
 }
