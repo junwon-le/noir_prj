3d68e0f8e96411edc6597618030928ec1eb10885
junwon-le <junwon2350@gmail.com>
2026-01-13 14:57:53 +0900

    wellcome noirhotel

---
+++ src/main/java/kr/co/noir/NoirPrjApplication.java
@@ -0,0 +1,13 @@
+package kr.co.noir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoirPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoirPrjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/noir/NoirPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.noir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoirPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
