1daf4d6fcbd4493dbad38169622b91abd306aab2
jy9s <jyeppp@gmail.com>
2026-01-15 15:19:06 +0900

    사이드바 변경

--- src/main/java/kr/co/noir/main/MainController.java
+++ src/main/java/kr/co/noir/main/MainController.java
@@ -23,11 +23,11 @@ public class MainController {
 	
 	@GetMapping("/admin/dinningMgr")
 	public String dinningMgr() {
-		return "admin_dinning/dinningManage";
+		return "manager/dinning/dinningManage";
 	}
 	@GetMapping("/admin/adminRoom")
 	public String admin_room() {
-		return "admin_room/roomManagePrice";
+		return "manager/room/roomManagePrice";
 	}
 	
 	@GetMapping("/room")
