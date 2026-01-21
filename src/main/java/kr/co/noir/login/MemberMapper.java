3282960237de880b120f9776d788b6681fbffbf9
cosmos <cosmos204@gmail.com>
2026-01-21 16:50:06 +0900

    findPw

--- src/main/java/kr/co/noir/login/MemberMapper.java
+++ src/main/java/kr/co/noir/login/MemberMapper.java
@@ -8,4 +8,5 @@ public interface MemberMapper {
 	// 아이디와 비밀번호로 관리자 조회
 	MemberDTO login(@Param("memberId") String memberId, @Param("memberPass") String memberPass);
 	MemberDTO findIdByNameAndEmail(@Param("memberName") String memberName, @Param("memberEmail") String memberEmail);
+	int updatePassword(@Param("memberId") String memberId, @Param("newPw") String newPw);
 }
