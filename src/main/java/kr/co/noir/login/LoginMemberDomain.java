3e8bb7e199eb121e8ba9d8c2946092c6b66df098
cosmos <cosmos204@gmail.com>
2026-01-27 17:47:35 +0900

    bscrypt

---
+++ src/main/java/kr/co/noir/login/LoginMemberDomain.java
@@ -0,0 +1,18 @@
+package kr.co.noir.login;
+
+import lombok.Data;
+
+@Data
+public class LoginMemberDomain {
+	private String memberId;    // MEMBER_ID
+	private String memberPass;  // MEMBER_PASS
+	private String memberLastName;  // MEMBER_NAME
+	private String memberFirstName;  // MEMBER_NAME
+	private String memberEmail; // MEMBER_EMAIL
+    private String memberTel;
+    private String memberBirth;
+    private String memberDelFlag;
+    private String memberInputDate;
+    private String memberIp;
+    private String resultMsg; // 결과 메시지
+}
