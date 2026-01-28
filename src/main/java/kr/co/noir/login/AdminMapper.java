59793f30d007f62ba9fe8c41b430bd493976fbf4
cosmos <cosmos204@gmail.com>
2026-01-28 10:06:26 +0900

    dupl remove

--- src/main/java/kr/co/noir/login/AdminMapper.java
+++ src/main/java/kr/co/noir/login/AdminMapper.java
@@ -6,5 +6,5 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface AdminMapper { 
 	// 아이디와 비밀번호로 관리자 조회
-	AdminDTO login(@Param("adminId") String adminId, @Param("adminPass") String adminPass);
+	AdminDTO adminLogin(@Param("adminId") String adminId, @Param("adminPass") String adminPass);
 }
