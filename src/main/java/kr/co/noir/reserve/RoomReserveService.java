c45b5d61f5057466713a1edebe61778a105c4244
junwon <junwon2350@gmail.com>
2026-02-01 23:42:22 +0900

    비회원 예약

--- src/main/java/kr/co/noir/reserve/RoomReserveService.java
+++ src/main/java/kr/co/noir/reserve/RoomReserveService.java
@@ -146,6 +146,70 @@ public class RoomReserveService {
 		}
 		return flag;
 	}//addRoomReserve
+	
+	public boolean addNonRoomReserve(PayInfoDTO pDTO, RoomReserveDTO rrDTO ) {
+		boolean flag = false;
+		
+		//예약한 날짜에 방이 있는 지 서버에서 검증
+		String startDate = rrDTO.getStartDate().replaceAll("\\(.*?\\)", "").trim();
+		String endDate = rrDTO.getEndDate().replaceAll("\\(.*?\\)", "").trim();
+		RoomSearchDTO rsDTO = new RoomSearchDTO();
+		rsDTO.setStartDate(startDate);
+		rsDTO.setEndDate(endDate);
+		List<RoomSearchDomain> list= searchRoomServer(rsDTO);
+		int[] roomArr = {0,0,0,0,0};
+		int roomNum =0;
+		int maxPerson =0;
+		for(int room : rrDTO.getRoom_type()) {
+			roomNum = room-1;
+			switch(room) {
+			case 1: roomArr[roomNum]+=1; break;
+			case 2: roomArr[roomNum]+=1; break;
+			case 3: roomArr[roomNum]+=1; break;
+			case 4: roomArr[roomNum]+=1; break;
+			case 5: roomArr[roomNum]+=1; break;
+			}//end switch
+			maxPerson += rDAO.selectRoom(room).getRoomMaxPerson();
+		}//end for
+		for(int i=0;i < roomArr.length; i++) {
+			if(roomArr[i]> list.get(i).getAvailable_count()) {
+				return flag;
+			}//end if
+		}//end for
+		//선택한 방에 인원 유효성 검증
+		if(maxPerson>rrDTO.getReserve_adult_count()) {
+			return flag;
+		}//end if
+		//어린이 인원 수 검증
+		if(rrDTO.getReserve_kid_count()>rrDTO.getRoom_type().size()*2) {
+			return flag;
+		}//end if
+		
+		
+		//예약 테이블 데이터 처리 - 서버에서 다시 db조회하여 실제 데이터를 가져옴
+		rrDTO.setRoom_res_startDate(LocalDate.parse(startDate));
+		rrDTO.setRoom_res_endDate(LocalDate.parse(endDate));
+		String email = rrDTO.getEmailId()+"@"+rrDTO.getEmailDomain();
+		rrDTO.setEmail(email);
+		//숙소 예약 테이블 데이터 처리
+		//결제 정보 데이터 처리
+		try {
+			// 빌링키 정보를 조회
+			var response = client.getBillingCustomer(pDTO.getBilling_key());
+			pDTO.setAgency(response.getResponse().getCardName());;//카드사
+			pDTO.setCard_number(response.getResponse().getCardNumber());//카드 번호 
+			pDTO.setPg_provider(response.getResponse().getPgProvider());//PG사 구분 코드
+		} catch (Exception e) {
+		}
+		try {
+			//예약 정보 저장 트렌젝션 처리
+			flag = rrDAO.insertNonRoomReserve(pDTO, rrDTO)==(4+rrDTO.getRoom_type().size());
+			System.out.println(flag);
+		}catch(PersistenceException pe) {
+			pe.printStackTrace();
+		}
+		return flag;
+	}//addNonomReserve
 
 	
 }
