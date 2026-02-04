d90e6e16d663dd564334970985328a1c8852d420
PARKHYUNWOOK-cloud <mellifluous5473@naver.com>
2026-02-04 22:43:42 +0900

    비회원예약 시작

---
+++ src/main/java/kr/co/noir/nonMemberReserve/NonUserContriller.java
@@ -0,0 +1,39 @@
+package kr.co.noir.nonMemberReserve;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/nonuser")
+@Controller
+public class NonUserContriller {
+
+	
+	
+	@GetMapping("/dinningList")
+	public String DinningListView() {
+		
+		return "/nonReserve/nonMemberDinningList";
+	}//DinningListView
+
+	@GetMapping("dinningDetail")
+	public String DinningRevDetail() {	
+		return "/nonReserve/nonMemberDinningDetail";
+	}//DinningRevDetail
+	
+	
+	@GetMapping("/hotelList")
+	public String HotelListView() {
+		
+		
+		return "/nonReserve/nonMemberHotelList";
+	}//HotelListView
+	
+	@GetMapping("/hotelDetail")
+	public String HotelRevDetail() {
+		
+		return "/nonReserve/nonMemberHotelRevDetail";
+	}//HotelRevDetail
+	
+	
+}//class
