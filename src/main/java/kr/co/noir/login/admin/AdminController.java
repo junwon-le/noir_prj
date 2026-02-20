828af886431e8389ac309f9fc287d993b3b84ff9
cosmos <cosmos204@gmail.com>
2026-02-20 17:45:47 +0900

    admin repair

--- src/main/java/kr/co/noir/login/admin/AdminController.java
+++ src/main/java/kr/co/noir/login/admin/AdminController.java
@@ -50,9 +50,9 @@ public class AdminController {
             HttpSession newSession = request.getSession(true);
             // 새로운 세션 생성
             newSession.setAttribute("adminId", aDTO.getAdminId());            
-            // 성공 시 관리자 메인(대시보드)으로 리다이렉트 -- 나중에 작업
+            newSession.setAttribute("adminNum", aDTO.getAdminNum());            
  
-            return "redirect:/dashBoard"; 
+            return "redirect:/admin/dashBoard"; 
         }
         
         // [실패] 서비스에서 담아온 메시지를 그대로 화면에 전달
